a606b6f1bfb136b5d82cf8fedfec5359f862588e
Mohd Afzal Ansari <afzalansaria660@gmail.com>
2025-02-12 17:18:14 +0530

    inital setup done

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all your React files
+    "./node_modules/flowbite/**/*.js", // Enable Flowbite components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
