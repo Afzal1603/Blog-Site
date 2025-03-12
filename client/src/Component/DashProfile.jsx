ba9e52acc232cd84874ba0c5cd37157c3156ba3d
Mohd Afzal Ansari <afzalansaria660@gmail.com>
2025-03-12 23:48:46 +0530

    completely added delete user functionality

--- client/src/Component/DashProfile.jsx
+++ client/src/Component/DashProfile.jsx
@@ -1,8 +1,10 @@
-import { Button, TextInput } from "flowbite-react";
+import { Button, Modal, TextInput } from "flowbite-react";
 import { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { Loader } from "lucide-react";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
 import {
   updateStart,
   updateSuccess,
@@ -14,6 +16,8 @@ const DashProfile = () => {
   const [loading, setLoading] = useState(false);
   const [imageUrl, setImageUrl] = useState(null);
   const [formData, setFormData] = useState({});
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
   const filePickerRef = useRef();
   const dispatch = useDispatch();
   const handleChange = (e) => {
@@ -76,9 +80,45 @@ const DashProfile = () => {
       console.log(error);
     }
   };
+  const handleDelete = async () => {
+    console.log(currentUser._id);
+    try {
+      const res = await axios.delete(
+        `http://localhost:5000/user/delete/${currentUser._id}`,
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${currentUser.token}`,
+          },
+        }
+      );
+      console.log(res.data);
+      dispatch(updateSuccess(null));
+      toast.success(res.data.message);
+      navigate("/signin");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleSignOut = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/signout",
+        {},
+        { withCredentials: true } // ✅ Ensures cookies are included
+      );
 
+      toast.success(res.data.message);
+      dispatch(updateSuccess(null));
+
+      navigate("/signin");
+    } catch (error) {
+      console.log("Logout failed:", error);
+    }
+  };
   return (
-    <div className="max-w-lg w-full mx-auto flex flex-col h-full">
+    <div className="max-w-lg w-full mx-auto flex flex-col h-full  ">
       <h1 className="text-center my-4 text-3xl">Profile</h1>
       <form onSubmit={handleUpdateProfile} className="flex flex-col mx-8 gap-3">
         <input
@@ -135,9 +175,50 @@ const DashProfile = () => {
         </Button>
       </form>
       <div className="flex justify-between text-red-500 mx-8 mt-2">
-        <span className="cursor-pointer hover:text-red-300">Delete User</span>
-        <span className="cursor-pointer hover:text-red-300">Sign Out</span>
+        <span
+          className="cursor-pointer hover:text-red-300"
+          onClick={() => setShowModal(true)}
+        >
+          Delete User
+        </span>
+        <span
+          className="cursor-pointer hover:text-red-300"
+          onClick={handleSignOut}
+        >
+          Sign Out
+        </span>
       </div>
+      {showModal && (
+        <Modal
+          show={showModal}
+          size="md"
+          onClose={() => setShowModal(false)}
+          popup={true}
+        >
+          <Modal.Header />
+          <Modal.Body>
+            <div className="text-center">
+              <HiOutlineExclamationCircle className="text-6xl text-gray-400 mx-auto mb-4" />
+              <h3 className="mb-5 text-lg font-normal text-gray-500">
+                Are you sure you want to delete your account?
+              </h3>
+              <div className="flex justify-center gap-4">
+                <Button color="failure" outline onClick={handleDelete}>
+                  Yes,I am sure
+                </Button>
+                <Button
+                  color="gray"
+                  outline
+                  onClick={() => setShowModal(false)}
+                >
+                  {" "}
+                  No,Cancel
+                </Button>
+              </div>
+            </div>
+          </Modal.Body>
+        </Modal>
+      )}
       <ToastContainer />
     </div>
   );
