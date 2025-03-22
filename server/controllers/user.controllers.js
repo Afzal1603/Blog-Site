ec96b03a6567b4a0e69f5c884e1df09676be7cbd
Mohd Afzal Ansari <afzalansaria660@gmail.com>
2025-03-22 22:17:37 +0530

    added get users funtionality in backend

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -85,4 +85,43 @@ const deleteUser = async (req, res, next) => {
   }
 };
 
-module.exports = { update, deleteUser };
+const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(500, "User is not authenticated"));
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+    const usersWithoutPass = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+    const totalUsers = await User.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    return res
+      .status(200)
+      .json({
+        success: true,
+        users: usersWithoutPass,
+        totalUsers,
+        lastMonthUsers,
+      });
+  } catch (error) {
+    return next(errorHandler(500, "Something went wrong"));
+  }
+};
+
+module.exports = { update, deleteUser, getUsers };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const userRouter = express.Router();
 
-const { update, deleteUser } = require("../controllers/user.controllers"); // Ensure this is correct
+const {
+  update,
+  deleteUser,
+  getUsers,
+} = require("../controllers/user.controllers"); // Ensure this is correct
 const { verifyToken } = require("../utils/verifyToken");
 
 userRouter.put("/update/:userid", verifyToken, update);
 userRouter.delete("/delete/:userid", verifyToken, deleteUser);
+userRouter.get("/getusers", verifyToken, getUsers);
 module.exports = userRouter;
