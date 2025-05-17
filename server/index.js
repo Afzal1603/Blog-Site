aa432adcd2b61facaaa4ef525328aae0a1383d4c
Mohd Afzal Ansari <afzalansaria660@gmail.com>
2025-05-17 17:12:42 +0530

    changes made

--- server/index.js
+++ server/index.js
@@ -13,13 +13,25 @@ const commentRouter = require("./routes/comment");
 const PORT = process.env.PORT || 5000;
 dbConnect();
 //-------------------------------------------------------------------------
+
 app.use(express.json());
+const allowedOrigins = [
+  "https://blog-site-eight-weld.vercel.app",
+  "http://localhost:5173",
+];
+
 app.use(
   cors({
-    origin: [
-      "https://blog-site-eight-weld.vercel.app",
-      "http://localhost:5173",
-    ],
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
